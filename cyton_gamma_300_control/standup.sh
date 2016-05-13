052a0a221ffb1e885b08dc1eaaa6692f6566450f
MerAARIZOU <aarizou_meriem@yahoo.fr>
2016-05-13 15:15:25 +0100

    first commit

---
+++ cyton_gamma_300_control/standup.sh
@@ -0,0 +1,8 @@
+rostopic pub -1 /cyton_gamma_300/shoulder_roll_position_controller/command std_msgs/Float64 -- -1.2
+rostopic pub -1 /cyton_gamma_300/shoulder_pitch_position_controller/command std_msgs/Float64 -- -1.2
+rostopic pub -1 /cyton_gamma_300/elbow_roll_position_controller/command std_msgs/Float64 -- -1.2
+rostopic pub -1 /cyton_gamma_300/elbow_pitch_position_controller/command std_msgs/Float64 -- -1.2
+rostopic pub -1 /cyton_gamma_300/elbow_yaw_position_controller/command std_msgs/Float64 -- -1.2
+rostopic pub -1 /cyton_gamma_300/wrist_pitch_position_controller/command std_msgs/Float64 -- -1.2
+rostopic pub -1 /cyton_gamma_300/wrist_roll_position_controller/command std_msgs/Float64 -- -1.2
+rostopic pub -1 /cyton_gamma_300/gripper_position_controller/command std_msgs/Float64 -- -1.2
